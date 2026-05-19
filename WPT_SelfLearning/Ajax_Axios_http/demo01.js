d52ea631f6b0fa260278cd879cb486b9f2c1947d
snehaladole1412 <snehaladole2003@gmail.com>
2026-05-19 13:21:53 +0530

    all wpt self uploaded

--- WPT_SelfLearning/Ajax_Axios_http/demo01.js
+++
@@ -1,6 +0,0 @@
-const math=require('./math01')
-
-console.log("Hello World")
-console.log(module)
-
-math.add(20,30);
--- WPT_SelfLearning/Ajax_Axios_http/math01.js
+++
@@ -1,8 +0,0 @@
-function add(n1,n2)
-{
-    const res=n1+n2;
-    console.log('Addition= '+res)
-   
-}
-
-module.exports={ add }
